2a1d82bfa5726770fb0f1386dba16b3bb2ffa654
Diego Vilhalva <diegorocha450@yahoo.com>
2025-11-03 13:16:59 -0300

    project-setup

---
+++ nuxt.config.ts
@@ -0,0 +1,47 @@
+import tailwindcss from "@tailwindcss/vite"
+import { repositoryName, apiEndpoint } from './slicemachine.config.json';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: 'Prismic + Nuxt Minimal Starter',
+      htmlAttrs: { lang: 'en' },
+      meta: [{ charset: 'utf-8' }, { name: 'appple-moile-web-app-title', content: 'Farbe' }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ],
+    }
+  },
+  css:['~/assets/css/index.css'],
+  vite:{ plugin:[
+    tailwindcss()
+  ]},
+  modules: ['@nuxt/eslint', '@nuxtjs/prismic', '@nuxt/fonts'],
+
+  prismic: {
+    endpoint: apiEndpoint || repositoryName,
+    preview: '/api/preview',
+    clientConfig: {
+      routes: [
+        {
+          type: 'page',
+          path: '/:uid',
+        },
+        {
+          type: 'page',
+          uid: 'home',
+          path: '/',
+        },
+      ]
+    }
+  },
+
+  compatibilityDate: '2025-07-16',
+})

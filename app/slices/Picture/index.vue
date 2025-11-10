70eef17fc8607389335bb4c43774f4ca03ea1417
Diego Vilhalva <diegorocha450@yahoo.com>
2025-11-10 10:24:47 -0300

    add prismic

---
+++ app/slices/Picture/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { Content } from "@prismicio/client";
+
+// The array passed to `getSliceComponentProps` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(
+  getSliceComponentProps<Content.PictureSlice>(),
+);
+</script>
+
+<template>
+  <section :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation"
+    class="grid xl:grid-cols-[3fr_2fr] ">
+    <figure class="contents">
+      <PrismicImage :field="slice.primary.picture" class="z-20 w-full row-span-2" loading="lazy" />
+      <figcaption class="px-4 pt-4 pb-16 rich-text" :class="{'xl:self-start':slice.variation === 'default' || slice.variation === 'button',
+        'xl:order-last xl:self-end':slice.variation === 'top',
+      }">
+        <PrismicRichText :field="slice.primary.caption" />
+      </figcaption>
+    </figure>
+    <figure
+    v-if="$prismic.isFilled.image(slice.primary.secondary_picture)"
+    class="flex flex-col "
+    :class="{'xl:self-end':slice.variation === 'default' || slice.variation === 'button',
+        'xl:order-start xl:self-end':slice.variation === 'top',
+      }"
+
+    >
+      <PrismicImage :field="slice.primary.secondary_picture" class="z-20 w-full" loading="lazy" />
+      <figcaption class="px-4 pt-4 p-6 rich-text"
+      :class="{
+        'xl:order-first': slice.variation === 'button'
+      }">
+        <PrismicRichText :field="slice.primary.secondary_caption" />
+      </figcaption>
+    </figure>
+  </section>
+</template>
--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -69,7 +69,7 @@ type ContentRelationshipFieldWithData<
   >;
 }[Exclude<TCustomType[number], string>["id"]];
 
-type PageDocumentDataSlicesSlice = RichTextSlice;
+type PageDocumentDataSlicesSlice = PictureSlice | RichTextSlice;
 
 /**
  * Content for Page documents
@@ -218,6 +218,200 @@ export type SettingsDocument<Lang extends string = string> =
   >;
 
 export type AllDocumentTypes = PageDocument | SettingsDocument;
+
+/**
+ * Primary content in *Picture → Default → Primary*
+ */
+export interface PictureSliceDefaultPrimary {
+  /**
+   * Picture field in *Picture → Default → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.default.primary.picture
+   * - **Documentation**: https://prismic.io/docs/fields/image
+   */
+  picture: prismic.ImageField<never>;
+
+  /**
+   * caption field in *Picture → Default → Primary*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.default.primary.caption
+   * - **Documentation**: https://prismic.io/docs/fields/rich-text
+   */
+  caption: prismic.RichTextField;
+
+  /**
+   * Secondary Picture (optional) field in *Picture → Default → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.default.primary.secondary_picture
+   * - **Documentation**: https://prismic.io/docs/fields/image
+   */
+  secondary_picture: prismic.ImageField<never>;
+
+  /**
+   * Secondary Caption (optional) field in *Picture → Default → Primary*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.default.primary.secondary_caption
+   * - **Documentation**: https://prismic.io/docs/fields/rich-text
+   */
+  secondary_caption: prismic.RichTextField;
+}
+
+/**
+ * Default variation for Picture Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slices
+ */
+export type PictureSliceDefault = prismic.SharedSliceVariation<
+  "default",
+  Simplify<PictureSliceDefaultPrimary>,
+  never
+>;
+
+/**
+ * Primary content in *Picture → Top → Primary*
+ */
+export interface PictureSliceTopPrimary {
+  /**
+   * Picture field in *Picture → Top → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.top.primary.picture
+   * - **Documentation**: https://prismic.io/docs/fields/image
+   */
+  picture: prismic.ImageField<never>;
+
+  /**
+   * caption field in *Picture → Top → Primary*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.top.primary.caption
+   * - **Documentation**: https://prismic.io/docs/fields/rich-text
+   */
+  caption: prismic.RichTextField;
+
+  /**
+   * Secondary Picture (optional) field in *Picture → Top → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.top.primary.secondary_picture
+   * - **Documentation**: https://prismic.io/docs/fields/image
+   */
+  secondary_picture: prismic.ImageField<never>;
+
+  /**
+   * Secondary Caption (optional) field in *Picture → Top → Primary*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.top.primary.secondary_caption
+   * - **Documentation**: https://prismic.io/docs/fields/rich-text
+   */
+  secondary_caption: prismic.RichTextField;
+}
+
+/**
+ * Top variation for Picture Slice
+ *
+ * - **API ID**: `top`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slices
+ */
+export type PictureSliceTop = prismic.SharedSliceVariation<
+  "top",
+  Simplify<PictureSliceTopPrimary>,
+  never
+>;
+
+/**
+ * Primary content in *Picture → Button → Primary*
+ */
+export interface PictureSliceButtonPrimary {
+  /**
+   * Picture field in *Picture → Button → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.button.primary.picture
+   * - **Documentation**: https://prismic.io/docs/fields/image
+   */
+  picture: prismic.ImageField<never>;
+
+  /**
+   * caption field in *Picture → Button → Primary*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.button.primary.caption
+   * - **Documentation**: https://prismic.io/docs/fields/rich-text
+   */
+  caption: prismic.RichTextField;
+
+  /**
+   * Secondary Picture (optional) field in *Picture → Button → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.button.primary.secondary_picture
+   * - **Documentation**: https://prismic.io/docs/fields/image
+   */
+  secondary_picture: prismic.ImageField<never>;
+
+  /**
+   * Secondary Caption (optional) field in *Picture → Button → Primary*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: picture.button.primary.secondary_caption
+   * - **Documentation**: https://prismic.io/docs/fields/rich-text
+   */
+  secondary_caption: prismic.RichTextField;
+}
+
+/**
+ * Button variation for Picture Slice
+ *
+ * - **API ID**: `button`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slices
+ */
+export type PictureSliceButton = prismic.SharedSliceVariation<
+  "button",
+  Simplify<PictureSliceButtonPrimary>,
+  never
+>;
+
+/**
+ * Slice variation for *Picture*
+ */
+type PictureSliceVariation =
+  | PictureSliceDefault
+  | PictureSliceTop
+  | PictureSliceButton;
+
+/**
+ * Picture Shared Slice
+ *
+ * - **API ID**: `picture`
+ * - **Description**: Picture
+ * - **Documentation**: https://prismic.io/docs/slices
+ */
+export type PictureSlice = prismic.SharedSlice<
+  "picture",
+  PictureSliceVariation
+>;
 
 /**
  * Primary content in *RichText → Default → Primary*
@@ -375,6 +569,14 @@ declare module "@prismicio/client" {
       SettingsDocument,
       SettingsDocumentData,
       AllDocumentTypes,
+      PictureSlice,
+      PictureSliceDefaultPrimary,
+      PictureSliceTopPrimary,
+      PictureSliceButtonPrimary,
+      PictureSliceVariation,
+      PictureSliceDefault,
+      PictureSliceTop,
+      PictureSliceButton,
       RichTextSlice,
       RichTextSliceDefaultPrimary,
       RichTextSliceFullscreenPrimary,
